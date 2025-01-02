e61a9fae51762aad38eea15e2f75a05187f1bae0
masterthuku <peterthuku706@gmail.com>
2025-01-03 01:36:38 +0300

    second update

---
+++ client/src/routes/listPage/listPage.jsx
@@ -0,0 +1,11 @@
+import { listData } from '../../lib/dummydata'
+import './listPage.scss'
+
+function ListPage(){
+  const data = listData
+  return (
+    <div className='listPage'>ListPage</div>
+  )
+}
+
+export default ListPage
